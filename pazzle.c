390bb58f1049641290fa7512b840f4d3bb5e32cd
kznrluk <kznr.luk@gmail.com>
2017-10-15 02:08:26 +0900

    ソースを整理

--- pazzle.c
+++ pazzle.c
@@ -7,156 +7,155 @@
 // 構造体 二次元配列にするか 変数にするか
 struct stage{
     int num[X][Y];
+    int count;
 };
 
-int firstentry(void);
-int outputhtml(struct stage, int count);
-int header(void);
-int footer(void);
-int outputdata(struct stage, int count);
+void fstgetin (struct stage *map);
+void scdgetin (struct stage *map, int, int);
+void moving (struct stage *map, int, int);
+int judge (struct stage *map, int, int, int, int, int);
+int outputhtml(struct stage);
+void header(void);
+void footer(void);
+int outputdata(struct stage);
 
 int main (int argc,char *argv[]){
-    // 各種宣言
-    int x, y, i, u, e, count;
-    int mx, my;
-    int size = X*Y;
-    int mapseed[size];
+    // 各種宣言 警告が出るので一応初期化しておく。
+    int i, u;
     struct stage map;
-    char buff[5];
-    printf("<!-- pazzle.cgi -->\n");
+    for(i=0; i<X; i++){
+        for(u=0; u<Y; u++){
+            map.num[i][u] = 0;
+        }
+    }
+    map.count = 0;
     // 引数がある場合、ファイルを読み込む。
-    if (argc >= 2){
-        printf("<!-- ファイル読み込み -->\n");
-        FILE *fd;
-        char fmap[32];
-        fd = fopen("./temp/stage.dat","r");
-        fgets(fmap,32,fd);
-
-        mapseed[0] = atoi(strtok(fmap, ","));
-        e = 1;
-        while(fmap != NULL){
-            mapseed[e] = atoi(strtok(NULL, ","));
-            if(e == size - 1){
-                count = atoi(strtok(NULL, ","));
-                break;
-            }
-            e++;
-        }
-        e = 0;
-        for(i=0; i<X; i++){
-            for(u=0; u<Y; u++){
-                map.num[i][u] = mapseed[e];
-                e++;
-            }
-        }
-        fclose(fd);
+    if (argc > 1){
+        int mx, my;
         mx = atoi(argv[1]);
         my = atoi(argv[2]);
-        // 動かす
-        for (i=0;i<X;i++){
-            for (u=0;u<Y;u++){
-                if (map.num[i][u] == -1){
-                    if (mx == i && my == u - 1 || mx == i && my == u + 1 || my == u && mx == i + 1 || my == u && mx == i - 1){
-                        map.num[i][u] = map.num[mx][my];
-                        map.num[mx][my] = -1;
-                    }
-                }
-            }
-        }
-        count++;
+        scdgetin(&map, mx, my);
     }
     // 引数がない場合、初回起動処理に移行。
     else {
-        // まず配列に0,1,2... と格納する。
-        for (i=0; i < size; i++){
-            mapseed[i] = i;
-        }
-        // それをシャッフルする。
-        for(int i = 0; i < size; i++) {
-            int j = rand()%size;
-            int t = mapseed[i];
-            mapseed[i] = mapseed[j];
-            mapseed[j] = t;
-        }
-        // バラバラになった数字を配列に格納する。
-        e = 0;
-        for(i=0; i<X; i++){
-            for(u=0; u<Y; u++){
-                map.num[i][u] = mapseed[e];
-                e++;
-            }
-        }
-        map.num[0][0] = -1;
+        fstgetin(&map);
     }
-    // 右上に空白を挿入する。
-
-    // Debug
-    printf("<!-- DEBUG # ");
-    for(i=0; i<X; i++){
-        for(u=0; u<Y; u++){
-            printf("%2d",map.num[i][u]);
-        }
-    }
-    printf(" -->\n");
-
-    // クリックされた場所の隣に空白配列(-1)があるかどうかをチェックする。
-    //if (checkarg()){    }
-    outputhtml(map,count);
-    // 現在状況をファイルへ出力
-    outputdata(map,count);
+    // HTMLの出力
+    outputhtml(map);
+    // ファイルの出力
+    outputdata(map);
     return 0;
 }
 
-// この処理は蛇足かも。
-int moving (struct stage map, int x, int y, int move){
+void fstgetin (struct stage *map){
+    int mapseed[X*Y];
+    int i, u, e, j , t;
+    // まず配列に0,1,2... と格納する。
+    for (i=0; i < X*Y; i++){
+        mapseed[i] = i;
+    }
+    // それをシャッフルする。
+    for(i=0; i < X*Y; i++) {
+        j = rand()%X*Y;
+        t = mapseed[i];
+        mapseed[i] = mapseed[j];
+        mapseed[j] = t;
+    }
+    // バラバラになった数字を配列に格納する。
+    for(e=0, i=0; i<X; i++){
+        for(u=0; u<Y; u++){
+            (*map).num[i][u] = mapseed[e];
+            e++;
+        }
+    }
+    // 右上を空白にする。
+    (*map).num[0][0] = -1;
+}
+
+void scdgetin (struct stage *map, int x, int y){
     int i, u;
-    // 隣り合った数が-1の時
-    // ネストの深さは闇の深さ。要改善。
+    // ファイルの読み込み
+    FILE *fd;
+    char fmap[32];
+    fd = fopen("./temp/stage.dat","r");
+    fgets(fmap, 32, fd);
+    fclose(fd);
+    // 保存した配列の内容を読み込み
+    (*map).num[0][0] = atoi(strtok(fmap, ","));
+    for(i=0; i<X; i++){
+        for(u=0; u<Y; u++){
+            (*map).num[i][u] = atoi(strtok(NULL, ","));
+        }
+    }
+    (*map).count = atoi(strtok(NULL, ",")) + 1;
+    // 配列を動かす
+    moving(map, x, y);
+}
+
+// 説明しよう！この関数は実行されると同時に指定された場所に隣接する空白に値を書き込む関数だ！
+void moving (struct stage *map, int x, int y){
+    int i, u;
     for (i=0;i<X;i++){
         for (u=0;u<Y;u++){
-            if (map.num[i][u] == -1){
-                if (x == i && y == u - 1 || x == i && y == u + 1 || y == u && x == i + 1 || y == u && x == i - 1){
-                    if (move){
-                        //移動処理
-                        return i,u;
-                    }
-                    else{
-                        return 0;
-                    }
+            if((*map).num[i][u] == -1 && judge(x, y, i, u, 0)){
+                (*map).num[i][u] = (*map).num[x][y];
+                (*map).num[x][y] = -1;
+            }
+        }
+    }
+}
+
+// ココらへんの書き方下手くそ
+int judge (struct stage *map, int x, int y, int i, int u, int getspace){
+    if(getspace){
+        int n, c;
+        for (n=0;n<X;n++){
+            for (c=0;c<Y;c++){
+                if((*map).num[i][u] == -1){
+                    i = n;
+                    u = c;
                 }
             }
         }
     }
+    if((x == i && y == u - 1) || (x == i && y == u + 1) || (y == u && x == i + 1) || (y == u && x == i - 1)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
-int outputhtml (struct stage map, int count){
-    //各種宣言
+int outputhtml (struct stage map){
+    // 各種宣言
     int i, u;
-    //ヘッダの出力
+    // ヘッダの出力
     header();
+    // ボディの出力
     printf("<table>\n");
-    //ボディの出力
     for (i=0; i<X; i++){
         printf("<tr>\n");
         for (u=0; u<Y; u++){
+            // 空白マスは空白表示
             if (map.num[i][u] == -1){
                 printf("<td></td>\n");
             }
-            else if (moving(map, i, u, 0)){
-                printf("<td><img src='./temp/slide-%d.png'></td>\n", map.num[i][u]);
+            // 動けない場所にはリンクを貼らない。
+            else if (judge(i, u, 0, 0, 1)){
+                printf("<td><a href='pazzle.cgi?%d?%d'><img src='./temp/slide-%d.png'></td>\n", i, u, map.num[i][u]);
             }
             else{
-                printf("<td><a href='pazzle.cgi?%d?%d'><img src='./temp/slide-%d.png'></td>\n", i, u, map.num[i][u]);
+                printf("<td><img src='./temp/slide-%d.png'></td>\n", map.num[i][u]);
             }
         }
     }
     printf("</table>\n");
-    printf("<h2>現在のスコア:%3d</h2>\n", count);
+    printf("<h2>現在のスコア:%3d</h2>\n", map.count);
     // フッタの出力
     footer();
 }
 
-int header(void){
+void header(void){
     printf("<!DOCTYPE html>\n");
     printf("<html>\r\n");
     printf("<meta charset=\"utf-8\">\n");
@@ -165,15 +164,13 @@ int header(void){
     printf("</head>\n");
     printf("<body>\n");
     printf("<h1>SlidePazzle with CGI/C</h1><br>\n");
-    return 0;
 }
 
-int footer(void){
+void footer(void){
     printf("</body>\n");
     printf("</html>\n");
-    return 0;
 }
-int outputdata(struct stage map, int count){
+int outputdata(struct stage map){
     int i, u;
     FILE *fp;
     fp = fopen("./temp/stage.dat","w");
@@ -182,7 +179,7 @@ int outputdata(struct stage map, int count){
             fprintf(fp, "%d,", map.num[i][u]);
         }
     }
-    fprintf(fp, "%d", count);
+    fprintf(fp, "%d", map.count);
     fclose(fp);
     return 0;
 }
