7ae81a18162a4c376901318f03fb52990e1cf861
kznrluk <kznr.luk@gmail.com>
2017-10-29 16:40:39 +0900

    トップページまで動作

--- webpro.js
+++ webpro.js
@@ -1,41 +1,46 @@
 function response(req, res) {
     function execpuzzle(){
-        exec('./puzzle.exe', function(err, stdout, stderr){
-            if (err){
-                console.log(err);
-                res.writeHead(200,{'Content-type': 'text/plain'});
-                res.write(err);
-                res.end();
-            } else {
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.write(stdout);
-                res.end();
+        exec('puzzle', function(error, stdout, stderr) {
+            if (error != null) {
+                console.log(error);
             }
+            console.log('Exec puzzle.exe');
+            res.writeHead(200,  {'Content-Type': 'text/html'});
+            res.write(stdout);
+            res.end();
         });
     }
-    function responseHTML(err, html) {
-        if (err) {
-            res.writeHead(404, {'Content-type': 'text/plain'});
-            res.write('File not found.');
-            res.end();
-        } else {
-            res.writeHead(200, {'Content-type': 'text/html'});
-            res.write(html);
-            res.end();
-        }
+    function responseCSS(err, css){
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(css);
+        res.end();
     }
-    execpuzzle();
-}
-   
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer();
-var exec = require('child_process').exec;
+    function responseIMG(err, png){
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.write(png);
+        res.end();
+    }
 
-server.on('connection', function(connection){
+    console.log('Requested: ' + req.url);
+
+    if(req.url == '/puzzle'){
+        execpuzzle();
+    } else if(req.url.indexOf('.css') != -1) {
+        fs.readFile('.'+req.url, 'utf-8', responseCSS);
+    } else if(req.url.indexOf('.png') != -1) {
+        fs.readFile('.'+req.url, responseCSS);
+    }
+}
+
+var http = require('http');
+var server = http.createServer();
+var fs = require('fs');
+var exec = require('child_process').exec;
+var iconv = require('iconv-lite');
+server.on('connection', function(){
     console.log('Connected.');
 });
 
 server.on('request', response);
-server.listen(1234);
-console.log('server started.');
+server.listen(3000);
+console.log('Server started.');
