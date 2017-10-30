21ae28789589026e84de0df67a4daaf7ac9839b3
kznrluk <kznr.luk@gmail.com>
2017-10-30 17:05:07 +0900

    ファイルの例外処理とバグを修正

--- webpro.js
+++ webpro.js
@@ -4,6 +4,7 @@ var cgipass = 'puzzle.exe';
 
 var http   = require('http');
 var server = http.createServer();
+var io     = require('socket.io').listen(server);
 var fs     = require('fs');
 var exec   = require('child_process').exec;
 var im     = require('imagemagick');
@@ -23,7 +24,17 @@ function response(req, res) {
         var args = req.url.split(/\+|\?/);
         console.log(args);
         if (args.length == 1){
-            exec(cgipass, function(error, stdout) {
+            exec(cgipass + ' kznr_luk', function(error, stdout) {
+                if (error != null) {
+                    console.log(error);
+                }
+                console.log('Exec puzzle binary');
+                res.writeHead(200,  {'Content-Type': 'text/html'});
+                res.write(stdout);
+                res.end();
+            });
+        } else if (args.length == 2){
+            exec(cgipass + ' ' + args[1], function(error, stdout) {
                 if (error != null) {
                     console.log(error);
                 }
@@ -33,7 +44,7 @@ function response(req, res) {
                 res.end();
             });
         } else {
-            exec(cgipass + ' ' + args[1] +' '+ args[2], function(error, stdout) {
+            exec(cgipass + ' ' + args[1] +' '+ args[2] + ' ' + args[3], function(error, stdout) {
                 if (error != null) {
                     console.log(error);
                 }
@@ -45,19 +56,34 @@ function response(req, res) {
         }
     }
     function responseCSS(err, css){
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(css);
-        res.end();
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(css);
+            res.end();
+        }
     }
     function responseIMG(err, png){
-        res.writeHead(200, {'Content-Type': 'image/png'});
-        res.write(png);
-        res.end();
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'image/png'});
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.write(png);
+            res.end();
+        }
     }
-    function responseWEBP(err, webp){
-        res.writeHead(200, {'Content-Type': 'image/webp'});
-        res.write(webp);
-        res.end();
+    function responseICO(err, favicon){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'image/x-icon'});
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'image/x-icon'});
+            res.write(favicon);
+            res.end();
+        }
     }
     function readUserMsg(){
         var body='';
@@ -130,7 +156,7 @@ function response(req, res) {
     console.log('Requested: ' + req.url);
 
     if(req.url == '/'){
-        fs.readFile('./index.html', 'utf-8', responseIndex);
+        fs.readFile('./entry.html', 'utf-8', responseIndex);
     } else if(req.url.indexOf('puzzle.cgi') != -1) {
         execpuzzle();
     } else if(req.url.indexOf('.css') != -1) {
@@ -139,10 +165,21 @@ function response(req, res) {
         fs.readFile('.'+req.url, responseIMG);
     } else if(req.url.indexOf('.webp') != -1) {
         fs.readFile('.'+req.url, responseWEBP);
+    } else if(req.url.indexOf('.ico') != -1) {
+        fs.readFile('.'+req.url, responseICO);
     } else if(req.url.indexOf('/wait') != -1) {
         readUserMsg();
+    } else {
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.end();
     }
 }
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('greeting', {message: 'hello'}, function (data) {
+        console.log('result: ' + data);
+    });
+});
 
 server.on('connection', function(){
 });
