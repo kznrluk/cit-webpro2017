51153f0d64dca6b5e656da92fdabf1cbef1304d2
kznrluk <kznr.luk@gmail.com>
2017-11-04 05:46:35 +0900

    ロード画面まで

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 var socket = io();
 
 socket.on('hello', function () {
-socket.emit('world');
+	socket.emit('world');
 });
 
 $(function () {
     $('form').submit(function(){
+        showLoading();
 		socket.emit('twitterid', $('#id').val());
-		$('#id').val('');
+        $('#id').val('');
 		return false;
     });
 });
+
+function showLoading(){
+    twid = document.getElementById("id").value;
+    document.getElementById("loading").style.display = "block";
+    document.getElementById("id").placeholder = twid;
+    document.getElementById("id").disabled = true;
+    
+}
