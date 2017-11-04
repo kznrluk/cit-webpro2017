009ae89f8cb09aebb6bb6c8c494a3a4f4b06aff6
kznrluk <kznr.luk@gmail.com>
2017-11-04 16:26:51 +0900

    デザインを変更

--- js/script.js
+++ js/script.js
@@ -6,17 +6,47 @@ socket.on('hello', function () {
 
 $(function () {
     $('form').submit(function(){
-        showLoading();
-		socket.emit('twitterid', $('#id').val());
-        $('#id').val('');
+        if(leadValue()){
+            alert("正しいTwitterIDを入力してください。")
+        } else {
+            showLoading();
+            socket.emit('twitterid', $('#id').val());
+            $('#id').val('');
+        }
 		return false;
     });
 });
 
+$(function() {
+    $('#id').on('keydown keyup keypress change', function() {
+        if ( $(this).val().length > 0 && !leadValue() ) {
+            $('#submit').prop('disabled', false);
+        } else {
+            $('#submit').prop('disabled', true);
+        }
+    });
+});
+
 function showLoading(){
-    twid = document.getElementById("id").value;
+    var twid = document.getElementById("id").value;
     document.getElementById("loading").style.display = "block";
     document.getElementById("id").placeholder = twid;
     document.getElementById("id").disabled = true;
-    
+}
+
+function leadValue(){
+    var twid = document.getElementById("id").value;
+    if(twid.match(/@|\s/)) {
+        $('#atmark').addClass('spin');
+        $('#atmarks').addClass('flash');
+        setTimeout(stopSpin, 1400);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function stopSpin(){
+    $('#atmark').removeClass('spin');
+    $('#atmarks').removeClass('flash');
 }
